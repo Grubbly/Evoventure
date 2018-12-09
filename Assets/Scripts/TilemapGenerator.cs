f2d04106e4339134c85da60a3d5c66eba678b0d1
unknown <orthogonaltrajectory@gmail.com>
2018-12-08 20:49:48 -0900

    Basic tilemap generator for 1000x1000 default

---
+++ Assets/Scripts/TilemapGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapGenerator : MonoBehaviour {
+
+    private int fileSaveCount = 0;
+
+    public Vector3Int tilemapSize = new Vector3Int(40, 40, 0);
+    public Tilemap map;
+    public Tile terrainTile;
+    public int width = 0;
+    public int height = 0;
+
+    private void Awake()
+    {
+        map = GameObject.Find("Grid").GetComponentInChildren<Tilemap>();
+        populateMap();
+    }
+
+    public void populateMap()
+    {
+        map.BoxFill(tilemapSize, terrainTile, 0, 0, width, height);
+    }
+
+}
