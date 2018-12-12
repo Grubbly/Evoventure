073604296467ee6b0c6fb7dee112b68ea78d4fae
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-12-12 00:41:08 -0900

    Updates json mapping model to include rotation and fixes 3d parser to use relative position

--- Assets/Map/Scripts/MapParser3D.cs
+++ Assets/Map/Scripts/MapParser3D.cs
@@ -20,6 +20,7 @@ public class MapObject
     public string filename;
 	public string category;
     public xyz position;
+	public int rotation;
 }
 
 [System.Serializable]
@@ -32,6 +33,8 @@ public class MapParser3D : MonoBehaviour {
 	public string mapFile = "Assets/Map/map.json";	
 	private MapHolder worldMap;
 
+	private List <GameObject> loadedObjects;
+
 	public void loadMapFromJson() {
         if(File.Exists(mapFile)) {
             string dataFile = File.ReadAllText(mapFile); 
@@ -42,23 +45,52 @@ public class MapParser3D : MonoBehaviour {
 		}
 	}
 
-	public void instantiateMapObject(MapObject mapObject) {
-		 GameObject mapObjectPrefab = GameObject.Instantiate((GameObject)Resources.Load("MapPrefabs/" + mapObject.category + "/3D/" + mapObject.filename));
-		 mapObjectPrefab.transform.position = new Vector3(mapObject.position.x, mapObject.position.y, mapObject.position.z);
+	public GameObject updateMapObject(MapObject mapObject, GameObject mapObjectPrefab) {
+		mapObjectPrefab.transform.rotation = gameObject.transform.rotation;
+		mapObjectPrefab.transform.Rotate(0, mapObject.rotation, 0);
+		mapObjectPrefab.transform.position = new Vector3(gameObject.transform.position.x + mapObject.position.x * Mathf.Sin(gameObject.transform.rotation.y), 
+		 												  gameObject.transform.position.y + mapObject.position.y, 
+														  gameObject.transform.position.z - mapObject.position.z * Mathf.Cos(gameObject.transform.rotation.x));
+		return mapObjectPrefab;
+	}
+
+	public GameObject instantiateMapObject(MapObject mapObject) {
+		GameObject mapObjectPrefab = GameObject.Instantiate((GameObject)Resources.Load("MapPrefabs/" + mapObject.category + "/3D/" + mapObject.filename));
+		return updateMapObject(mapObject, mapObjectPrefab);
 	}
 
 	public void instantiateMap() {
 		foreach (MapObject mapObject in worldMap.objects) {
-			instantiateMapObject(mapObject);
+			GameObject instantiated = instantiateMapObject(mapObject);
+			loadedObjects.Add(instantiated);
+		}
+	}
+
+	public void updateMap() {
+		for (int i = 0; i < worldMap.objects.Count; ++i) {
+			if (worldMap.objects.Count == loadedObjects.Count)
+				updateMapObject(worldMap.objects[i], loadedObjects[i]);
+			else {
+				if (i < loadedObjects.Count) {
+					Destroy(loadedObjects[i]);
+					loadedObjects[i] = instantiateMapObject(worldMap.objects[i]);
+				}
+				else {
+					GameObject instantiated = instantiateMapObject(worldMap.objects[i]);
+					loadedObjects.Add(instantiated);
+				}
+			}
 		}
 	}
 
 	void Start () {
+		loadedObjects = new List<GameObject>();
 		loadMapFromJson();
 		instantiateMap();
 	}
 	
 	void Update () {
-		
+		loadMapFromJson();
+		updateMap();
 	}
 }
