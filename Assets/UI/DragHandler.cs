4a8abb38c0b5b36028d6540eb6261970e7d5cdb3
Rachel Crosley <rachelcrosley@Rachels-MacBook-Pro.local>
2018-12-13 00:57:47 -0900

    adding UI and beginning title screen

---
+++ Assets/UI/DragHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public static GameObject itemBeingDragged;
+	Vector3 startPos;
+	Transform startParent;
+
+    #region IBeginDragHandler implementation
+
+	public void OnBeginDrag(PointerEventData eventdata) {
+		itemBeingDragged = gameObject;
+		startPos = transform.position;
+		startParent = transform.parent;
+		GetComponent<CanvasGroup>().blocksRaycasts = false;
+	}
+    #endregion
+
+    #region IDragHandler implementation
+
+	public void OnDrag(PointerEventData eventdata) {
+		transform.position = Input.mousePosition;
+	}
+    #endregion
+
+    #region IEndDragHandler implementation
+
+    public void OnEndDrag(PointerEventData eventdata) {
+        itemBeingDragged = null;
+		GetComponent<CanvasGroup>().blocksRaycasts = true;
+		if(transform.parent == startParent) {
+            transform.position = startPos;
+		}
+        
+    }
+    #endregion
+
+}
---
+++ Assets/UI/StartButtonScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Button))]
+public class StartButtonScript : MonoBehaviour
+{
+
+    private Button button;
+
+    // Use this for initialization
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(StartGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
