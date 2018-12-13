50c0b5e44f1542d7b2ee09e0b5dd2f9fce57a6a3
Rachel Crosley <rachelcrosley@Rachels-MacBook-Pro.local>
2018-12-13 11:44:03 -0900

    added new icons to UI

---
+++ Assets/UI/UIManage.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManage : MonoBehaviour {
+
+    public GameObject inventoryScreen;
+
+    private void Awake()
+    {
+        inventoryScreen.SetActive(false);
+    }
+
+    public void LateUpdate()
+    {
+        checkPause();
+    }
+
+    void checkPause()
+    {
+        if (Input.GetKeyUp(KeyCode.I))
+        {
+            pauseGame(inventoryScreen.activeInHierarchy);
+        }
+    }
+
+    void pauseGame(bool active)
+    {
+        Time.timeScale = System.Convert.ToSingle(active);
+        inventoryScreen.SetActive(!active);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
