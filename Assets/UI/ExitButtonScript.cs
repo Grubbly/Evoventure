ad5e942fb01c5b6420034ea377df3dcaa18d60ab
Rachel Crosley <rachelcrosley@Rachels-MacBook-Pro.local>
2018-12-13 02:19:13 -0900

    Pressing start button starts game

--- Assets/UI/ExitButtonScript.cs
+++ Assets/UI/ExitButtonScript.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class ExitButtonScript : MonoBehaviour
+public class ExitOnClick : MonoBehaviour
 {
     public void ExitGame()
     {
