f242acbbfc41b732fb664f5001d348ea4a4a650e
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-12-12 20:22:57 -0900

    Adds set map mode to make placing map items easier

--- Assets/Map/Scripts/MapParser3D.cs
+++ Assets/Map/Scripts/MapParser3D.cs
@@ -31,8 +31,9 @@ public class MapHolder
 
 public class MapParser3D : MonoBehaviour {
 	public string mapFile = "Assets/Map/map.json";	
+	public string mapPrefabsLocation = "Assets/resources/MapPrefabs";
+	public bool setMapMode = true;
 	private MapHolder worldMap;
-
 	private List <GameObject> loadedObjects;
 
 	public void loadMapFromJson() {
@@ -83,14 +84,103 @@ public class MapParser3D : MonoBehaviour {
 		}
 	}
 
+	public string findFolderFor(string fileName) {
+		string [] categories = Directory.GetDirectories(mapPrefabsLocation);
+
+		foreach (string category in categories) {
+			string [] categoryPath = category.Split('/');
+			string categoryName = categoryPath[categoryPath.Length - 1];
+			string category3DPath = mapPrefabsLocation + "/" + categoryName  + "/3D";
+			if (Directory.GetFiles(category3DPath, fileName + ".*").Length > 0)
+				return categoryName;
+		}
+
+		return "";
+	}
+
+	public MapObject formMapObject(GameObject child) {
+		MapObject instantiated = new MapObject();
+
+		string fName = child.name.Split(' ')[0];
+		xyz position = new xyz();
+		position.x = (int)((child.transform.position.x - gameObject.transform.position.x)/Mathf.Sin(gameObject.transform.rotation.y));
+		position.z = (int)((child.transform.position.z - gameObject.transform.position.z)/Mathf.Cos(gameObject.transform.rotation.x));
+		position.y = (int)(child.transform.position.y  - gameObject.transform.position.y);
+		int rotation = (int)(child.transform.rotation.eulerAngles.y - gameObject.transform.rotation.eulerAngles.y) - 180;
+		string category = findFolderFor(fName);
+
+		instantiated.filename = fName;
+		instantiated.category = category;
+		instantiated.position = position;
+		instantiated.rotation = rotation;
+
+		return instantiated;
+	}
+
+	public void captureMap() {
+		worldMap = new MapHolder();
+		worldMap.objects = new List<MapObject>();
+         foreach (Transform child in transform)
+         {
+			loadedObjects.Add(child.gameObject);
+			MapObject childMapObject = formMapObject(child.gameObject);
+			worldMap.objects.Add(childMapObject);
+         }
+	}
+
+	public string stringifyMapObject(MapObject mapObject) {
+		string mapObjectJSON = "{\n";
+
+		mapObjectJSON += @"""filename"": """ + mapObject.filename + "\",\n";
+		mapObjectJSON += @"""category"": """ + mapObject.category + "\",\n";
+		mapObjectJSON += @"""position"": {" + "\n\"x\": " + mapObject.position.x + ",\n\"y\": " + mapObject.position.y + ",\n\"z\": " + -mapObject.position.z + "\n" + "},\n";
+		mapObjectJSON += @"""rotation"": """ + mapObject.rotation + "\"\n";
+
+		mapObjectJSON += "}";
+
+		return mapObjectJSON;
+	}
+
+	public string stringifyMap() {
+		string mapJSON = "{\n\"objects\": [\n";
+
+		bool firstMapObject = true;
+		mapJSON += stringifyMapObject(worldMap.objects[0]);
+
+		foreach(MapObject mapObject in worldMap.objects) {
+			if (firstMapObject)
+				firstMapObject = false;
+			else
+				mapJSON +=  ",\n" + stringifyMapObject(mapObject);
+		}
+
+		mapJSON += "\n]\n}";
+
+		return mapJSON;
+	}
+
+	public void writeMapToJSON() {
+		string worldMapJSON = stringifyMap();
+		System.IO.File.WriteAllText(mapFile, worldMapJSON);
+	}
+
 	void Start () {
 		loadedObjects = new List<GameObject>();
-		loadMapFromJson();
-		instantiateMap();
+		if (setMapMode) {
+			captureMap();
+			writeMapToJSON();
+		}
+		else {
+			loadMapFromJson();
+			instantiateMap();
+		}
+		
 	}
 	
 	void Update () {
-		loadMapFromJson();
-		updateMap();
+		if (!setMapMode) {
+			loadMapFromJson();
+			updateMap();
+		}
 	}
 }
